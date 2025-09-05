0df245b7ba9b1f17e0c0224fe4a16f2d707ec02d
papacharae-gr <papacharae.gr@gmail.com>
2025-09-05 09:19:08 +0300

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { ChakraProvider, createSystem, defaultConfig } from '@chakra-ui/react'
+
+const system = createSystem(defaultConfig)
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ChakraProvider value={system}>
+      <App />
+    </ChakraProvider>
+  </StrictMode>,
+)
