1d0c39237fb9eddfef205d362919f158e1db2bf6
papacharae-gr <papacharae.gr@gmail.com>
2025-09-09 10:29:01 +0300

    -

--- src/features/listings/index.ts
+++ src/features/listings/index.ts
@@ -1,3 +1,20 @@
+// Feature pages
 export { default as ListingUpdatePage } from './feature/update/ListingUpdatePage';
+export { default as ListingViewPage } from './feature/listOne/ListingViewPage';
+export { default as ListingDetails } from './feature/listOne/ListingDetails';
+export { default as HotelListPage } from './feature/listAll/HotelListPage';
+export { HotelList } from './feature/listAll/HotelList';
+
+// Components
+export { HotelCard } from './components/HotelCard';
+
+// Data-access
 export * from './data-access/useListingQuery';
+export * from './data-access/useListHotelsQuery';
+export * from './data-access/useDeleteHotelMutation';
+export * from './data-access/useUpdateListingMutation';
+export * from './data-access/useListingUpdateHandler';
 export * from './data-access/gateway/listing.gateway';
+
+// Domain model
+export * from './domain/listing.model';
